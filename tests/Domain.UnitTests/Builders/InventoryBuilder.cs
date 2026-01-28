8b57263b97241ea340aee3f36cb558d79062dad4
Mostafa Dev <Mostafa.Medhat.0841@gmail.com>
2026-01-28 20:03:33 +0200

    feat(inventory) add unit tests for inventory aggregate.

---
+++ tests/Domain.UnitTests/Builders/InventoryBuilder.cs
@@ -0,0 +1,69 @@
+﻿using IMS.Domain.Inventories;
+
+namespace Domain.UnitTests.Builders
+{
+    public class InventoryBuilder
+    {
+
+        //Define default values for the Inventory.
+        // Builder Create()
+        // builder With[Property_Name](Property_Value)
+        // Result<Inventory> Build()
+        // BuildSuccessfully Result.IsSuccess => returns the Inventory: Result.IsFailure => throws Exception
+
+        private int _quantity = 12;
+
+        public int _lowStockThreshold = 10;
+
+        public Guid _productId = Guid.NewGuid();
+
+
+
+        public static InventoryBuilder Create() => new();
+
+
+        public static InventoryBuilder CreateInvalid() => new InventoryBuilder
+        {
+            _quantity = 0,
+            _lowStockThreshold = 0,
+            _productId = Guid.Empty
+        };
+
+        public InventoryBuilder WithQuantity(int quantity)
+        {
+            _quantity = quantity;
+            return this;
+        }
+        public InventoryBuilder WithLowStockThreshold(int lowStockThreshold)
+        {
+            _lowStockThreshold = lowStockThreshold;
+            return this;
+        }
+        public InventoryBuilder WithProductId(Guid productId)
+        {
+            _productId = productId;
+            return this;
+        }
+
+        public Result<Inventory> Build()
+        {
+            return Inventory.Create(_quantity, _lowStockThreshold, _productId);
+        }
+
+        public Inventory BuildSuccessfully()
+        {
+            var result = Build();
+
+            if (result.IsSuccess)
+
+                return result.Value!;
+            else
+
+                throw new InvalidOperationException(
+               $"Failed to build inventory: {result.Error!.Description}");
+
+
+        }
+
+    }
+}
