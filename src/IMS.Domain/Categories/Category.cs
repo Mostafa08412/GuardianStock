3aff94fe7d0e07a7b417694c4e9eddc618a1d2c3
Mostafa Dev <Mostafa.Medhat.0841@gmail.com>
2026-01-28 02:00:33 +0200

    feat(category): add category aggregate with defined errors

---
+++ src/IMS.Domain/Categories/Category.cs
@@ -0,0 +1,91 @@
+﻿using IMS.Domain.Abstractions;
+using IMS.Domain.Core.Primitives;
+using IMS.Domain.Core.Primitives.Result;
+
+namespace IMS.Domain.Categories
+{
+    public class Category : Aggregate, IAuditable
+    {
+        protected Category()
+        {
+        }
+
+        public Category(Guid id, string name, string description) : base(id)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+
+        public DateTime CreatedOnUTC { get; private set; }
+
+        public string CreatedBy { get; private set; }
+
+        public DateTime UpdatedOnUTC { get; private set; }
+
+        public string UpdatedBy { get; private set; }
+
+
+        public static Result<Category> Create(string name, string description)
+        {
+            var newId = Guid.CreateVersion7();
+
+
+            if (string.IsNullOrEmpty(name))
+            {
+                return Result<Category>.Failure(Errors.CategoryErrors.NameIsRequired);
+            }
+
+            if (string.IsNullOrEmpty(description))
+            {
+                return Result<Category>.Failure(Errors.CategoryErrors.DescriptionIsRequired);
+            }
+
+            var newCategory = new Category(newId, name, description);
+
+            return Result<Category>.Success(newCategory);
+        }
+
+        public Result Rename(string newName)
+        {
+            if (string.IsNullOrEmpty(newName))
+            {
+                return Result<Category>.Failure(Errors.CategoryErrors.NameIsRequired);
+            }
+
+            Name = newName;
+
+            return Result.Success();
+
+        }
+
+        public Result Redescription(string newDescription)
+        {
+            if (string.IsNullOrEmpty(newDescription))
+            {
+                return Result<Category>.Failure(Errors.CategoryErrors.DescriptionIsRequired);
+            }
+
+            Description = newDescription;
+
+            return Result.Success();
+
+        }
+
+    }
+
+    public static class Errors
+    {
+
+        public static class CategoryErrors
+        {
+            public static Error NameIsRequired => new Error("Category.NameIsRequired", "Category name is required", ErrorType.Validation);
+            public static Error DescriptionIsRequired => new Error("Category.DescriptionIsRequired", "Category description is required", ErrorType.Validation);
+
+        }
+
+    }
+}
