738843cb4ef65457a95335e5302b638b25c7b665
Mostafa Dev <Mostafa.Medhat.0841@gmail.com>
2026-01-28 04:20:55 +0200

    feat(inventory) add inventory aggregate, low stock alert value object and defined (validation/business rule) errors.

---
+++ src/IMS.Domain/Inventories/Inventory.cs
@@ -0,0 +1,196 @@
+﻿using IMS.Domain.Abstractions;
+using IMS.Domain.Core.Primitives;
+using IMS.Domain.Core.Primitives.Result;
+
+namespace IMS.Domain.Inventories
+{
+    public class Inventory : Aggregate, IAuditable
+    {
+
+        protected Inventory() : base()
+        {
+
+        }
+
+        public Inventory(Guid id, int quantity, int lowStockThreshold, Guid productId) : base(id)
+        {
+            Quantity = quantity;
+            LowStockThreshold = lowStockThreshold;
+            ProductId = productId;
+
+        }
+
+        public int Quantity { get; private set; }
+
+        public int LowStockThreshold { get; private set; }
+
+        public Guid ProductId { get; private set; }
+
+        public LowStockAlert? LowStockAlert { get; private set; }
+
+        public InventoryStatus Status
+        {
+
+            get
+            {
+                decimal quantity = new Decimal(this.Quantity);
+                decimal lowStockThreshold = new Decimal(this.LowStockThreshold);
+
+                decimal ratio = quantity / lowStockThreshold;
+
+                if (ratio <= 0.3m) return InventoryStatus.Critical;
+                else if (ratio > 0.3m && ratio <= 1.0m) return InventoryStatus.Low;
+                else return InventoryStatus.Healthy;
+
+            }
+
+        }
+
+        public DateTime CreatedOnUTC { get; private set; }
+
+        public string CreatedBy { get; private set; }
+
+        public DateTime UpdatedOnUTC { get; private set; }
+
+        public string UpdatedBy { get; private set; }
+
+
+        public static Result<Inventory> Create(int quantity, int lowStockThreshold, Guid productId)
+        {
+            var newId = Guid.CreateVersion7();
+
+
+            if (quantity < 1)
+            {
+                return Result<Inventory>.Failure(Errors.InventoryErrors.InvalidQuantity);
+            }
+
+            if (lowStockThreshold < 10)
+            {
+                return Result<Inventory>.Failure(Errors.InventoryErrors.InvalidLowStockThreshold);
+            }
+
+            if (productId == Guid.Empty)
+            {
+                return Result<Inventory>.Failure(Errors.InventoryErrors.InvalidLowStockThreshold);
+            }
+
+            var newInventory = new Inventory(newId, quantity, lowStockThreshold, productId);
+
+            return Result<Inventory>.Success(newInventory);
+        }
+
+        public Result Restock(int addedQuantity)
+        {
+            if (addedQuantity <= 0)
+            {
+                return Result.Failure(Errors.InventoryErrors.InvalidRestockQuantity);
+            }
+
+            Quantity += addedQuantity;
+
+            // Action Taken: Inventory is Restocked, the alert is gone.
+            if (LowStockAlert != null && Quantity > LowStockAlert.Threshold)
+                LowStockAlert = null;
+
+
+
+            return Result.Success();
+
+        }
+
+        public Result Ship(int shippedQuantity)
+        {
+            if (shippedQuantity <= 0)
+            {
+                return Result.Failure(Errors.InventoryErrors.InvalidShipQuantity);
+            }
+
+            if (shippedQuantity > Quantity)
+            {
+                return Result.Failure(Errors.InventoryErrors.InsufficientStock);
+            }
+
+            Quantity -= shippedQuantity;
+
+            if (Quantity <= LowStockThreshold)
+            {
+
+                if (LowStockAlert == null)
+                {
+                    LowStockAlert = LowStockAlert.Trigger(LowStockThreshold);
+                    //Trigger a LowStockAlertTriggered Domain Event
+                }
+                // Case 2: Means the alert is triggered but still not being sent.. (awaiting notification send confirmation)
+                else if (!LowStockAlert.IsNotificationSent)
+                {
+
+                }
+
+                // Case 3: Means the alert is already triggered once before and sent, but it hasn't been solved yet.
+                // we ignore this case because according to BR, we should send the alert 1 time only,
+                // till a new stock above the threshold is done..
+            }
+
+
+
+            return Result.Success();
+
+        }
+
+        public Result AdjustLowStockThreshold(int newLowStockThreshold)
+        {
+            if (newLowStockThreshold < 10)
+            {
+                return Result.Failure(Errors.InventoryErrors.InvalidLowStockThreshold);
+            }
+
+            if (LowStockAlert != null && LowStockAlert.HasPendingAction)
+                return Result.Failure(Errors.InventoryErrors.CannotAdjustThresholdWithActiveAlert);
+
+            LowStockThreshold = newLowStockThreshold;
+
+
+            LowStockAlert = null;
+
+            if (Quantity <= LowStockThreshold)
+            {
+                LowStockAlert = LowStockAlert.Trigger(newLowStockThreshold);
+            }
+
+            return Result.Success();
+        }
+        public void ConfirmNotificationSent()
+        {
+            if (LowStockAlert != null && !LowStockAlert.IsNotificationSent)
+            {
+                LowStockAlert = LowStockAlert.MarkAsSent();
+            }
+        }
+
+
+    }
+
+    public static class Errors
+    {
+
+        public static class InventoryErrors
+        {
+            public static Error InvalidQuantity => new Error("Inventory.InvalidQuantity", "Inventory quantity must be greater than 1.", ErrorType.Validation);
+            public static Error InvalidLowStockThreshold => new Error("Inventory.InvalidLowStockThreshold", "Low stock threshold minimum is 10.", ErrorType.Validation);
+            public static Error ProductIsRequired => new Error("Inventory.ProductIsRequired", "Inventory product is required.", ErrorType.Validation);
+
+            public static Error InvalidShipQuantity => new Error("Inventory.InvalidShipQuantity", "The quantity to ship must be greater than zero.", ErrorType.Validation);
+
+            public static Error InvalidRestockQuantity => new Error("Inventory.InvalidRestockQuantity", "The quantity to restock must be greater than zero.", ErrorType.Validation);
+
+            public static Error InsufficientStock = new("Inventory.InsufficientStock", "The available quantity in stock is not enough to fulfill this request.", ErrorType.ConditionNotMet);
+
+            public static Error CannotAdjustThresholdWithActiveAlert => new(
+            "Inventory.CannotAdjustThresholdWithActiveAlert",
+            "The low stock threshold cannot be adjusted while there is an active alert. Please dismiss or reset the alert first.",
+            ErrorType.ConditionNotMet);
+        }
+
+    }
+}
---
+++ src/IMS.Domain/Inventories/LowStockAlert.cs
@@ -0,0 +1,44 @@
+﻿using IMS.Domain.Core.Primitives;
+
+namespace IMS.Domain.Inventories
+{
+    public class LowStockAlert : ValueObject
+    {
+        public LowStockAlert(int threshold, DateTime? notificationSentAt, DateTime? dismissedAt, DateTime triggeredAtUTC)
+        {
+            Threshold = threshold;
+            NotificationSentAt = notificationSentAt;
+            DismissedAt = dismissedAt;
+            TriggeredAtUTC = triggeredAtUTC;
+        }
+
+        public int Threshold { get; private set; }
+
+        public DateTime? NotificationSentAt { get; private set; }
+
+        public DateTime? DismissedAt { get; private set; }
+
+        public DateTime? TriggeredAtUTC { get; private set; }
+
+        public bool IsNotificationSent => NotificationSentAt != null;
+
+        public bool HasPendingAction => !DismissedAt.HasValue && IsNotificationSent;
+
+
+
+        public static LowStockAlert Trigger(int threshold) => new(threshold, null, null, DateTime.UtcNow);
+        public LowStockAlert MarkAsSent() => new(Threshold, DateTime.UtcNow, null, TriggeredAtUTC!.Value);
+        public LowStockAlert Dismiss() => new(Threshold, NotificationSentAt, DateTime.Now, TriggeredAtUTC!.Value);
+        public LowStockAlert Reset() => null;
+
+        public LowStockAlert ResetWithNewThreshold(int newThreshold) => new(newThreshold, null, null, default);
+        public override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Threshold;
+            yield return NotificationSentAt;
+            yield return DismissedAt;
+            yield return TriggeredAtUTC;
+
+        }
+    }
+}
