7011a756a54c4d7df6a1a6ef87ae87093b29a7ad
Mostafa Dev <Mostafa.Medhat.0841@gmail.com>
2026-01-30 10:11:04 +0200

    feat(category): add configuration for category aggregate

---
+++ src/IMS.Infrastructure/Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using IMS.Domain.Categories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IMS.Infrastructure.Persistence.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+
+            builder
+                .HasIndex(X => X.Name)
+                .IsUnique();
+
+            builder.Property(X => X.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(X => X.Description)
+               .IsRequired()
+               .HasMaxLength(500);
+
+        }
+    }
+}
