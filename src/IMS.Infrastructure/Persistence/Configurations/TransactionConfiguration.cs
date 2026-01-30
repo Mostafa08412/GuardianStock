68991b6c1df380cf4cdbe2c6151ff126a0301f5f
Mostafa Dev <Mostafa.Medhat.0841@gmail.com>
2026-01-30 11:47:25 +0200

    feat(transaction): add configuration for transaction aggregate.

---
+++ src/IMS.Infrastructure/Persistence/Configurations/TransactionConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using IMS.Domain.Products;
+using IMS.Domain.Transactions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IMS.Infrastructure.Persistence.Configurations
+{
+    internal class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.ToTable("Transactions");
+
+            builder
+                .HasIndex(X => new { X.ProductId, X.CreatedOnUTC })
+                .IsUnique();
+
+            builder
+                 .HasOne<Product>().WithMany()
+                 .HasForeignKey(X => X.ProductId);
+
+
+        }
+    }
+}
