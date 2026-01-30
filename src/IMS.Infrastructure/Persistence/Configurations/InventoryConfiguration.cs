9c12b0951199840144d1f8b537adcbd2cf92ea4e
Mostafa Dev <Mostafa.Medhat.0841@gmail.com>
2026-01-30 11:46:57 +0200

    feat(inventory): add configuration for inventory aggregae.

---
+++ src/IMS.Infrastructure/Persistence/Configurations/InventoryConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using IMS.Domain.Inventories;
+using IMS.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IMS.Infrastructure.Persistence.Configurations
+{
+    internal class InventoryConfiguration : IEntityTypeConfiguration<Inventory>
+    {
+        public void Configure(EntityTypeBuilder<Inventory> builder)
+        {
+            builder.ToTable("Inventories");
+
+            builder
+                .HasIndex(X => X.ProductId)
+                .IsUnique();
+
+            builder.OwnsOne(X => X.LowStockAlert);
+
+            builder
+                .HasOne<Product>().WithOne()
+                .HasForeignKey<Inventory>(X => X.ProductId);
+
+        }
+    }
+}
