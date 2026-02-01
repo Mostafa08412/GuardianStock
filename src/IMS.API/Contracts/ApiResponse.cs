a0c44a230cbd753ab6c20c6faf98d786ed4c7f8f
Mostafa Dev <Mostafa.Medhat.0841@gmail.com>
2026-02-01 13:44:22 +0200

    refactor(api): refactor api response class.

--- src/IMS.API/Contracts/ApiResponse.cs
+++ src/IMS.API/Contracts/ApiResponse.cs
@@ -1,64 +1,36 @@
 ﻿namespace IMS.API.Contracts
 {
-
-    public abstract class ApiResponse
+    public record ApiResponse
     {
-        public abstract int StatusCode { get; }
 
-        public bool IsSuccess => StatusCode >= 200 || StatusCode >= 300;
+        public bool IsSuccess { get; private set; }
+        public string Message { get; private set; }
+        public string ErrorCode { get; private set; }
+        public IDictionary<string, string> ValidationErrors { get; private set; }
+        public IDictionary<string, string> Meta { get; private set; }
+        public string Instance { get; private set; }
+        public string TraceId { get; private set; }
+
+        public ApiResponse(bool isSucess, string message, string errorCode, IDictionary<string, string> validationErrors, IDictionary<string, string> meta, string instance, string traceId)
+        {
+            IsSuccess = isSucess;
+            Message = message;
+            ErrorCode = errorCode;
+            ValidationErrors = validationErrors;
+            Meta = meta;
+            Instance = instance;
+            TraceId = traceId;
+        }
     }
-    public class OkApiResponse<T> : ApiResponse
+
+    public record ApiResponse<T> : ApiResponse
     {
-
-        public override int StatusCode => 200;
-
-        public T Data { get; init; }
-
-        public OkApiResponse(T data)
+        public ApiResponse(bool isSucess, string message, string errorCode, IDictionary<string, string> validationErrors, IDictionary<string, string> meta, string instance, string traceId, T data) :
+            base(isSucess, message, errorCode, validationErrors, meta, instance, traceId)
         {
             Data = data;
         }
-    }
-    public class PaginatedApiResponse<T> : OkApiResponse<T>
-    {
-        public PaginatedApiResponse(T data, int totalItems, int pageNo, int pageSize, int totalPages) : base(data)
-        {
-            TotalPages = totalPages;
-            TotalItems = totalItems;
-            PageNo = pageNo;
-            PageSize = pageSize;
-
-
-        }
-
-        public int PageNo { get; init; }
-        public int PageSize { get; init; }
-        public int TotalPages { get; init; }
-
-        public int TotalItems { get; init; }
-
-
+        public T Data { get; private set; }
 
     }
-
-    public class NoContentApiResponse : ApiResponse
-    {
-        public override int StatusCode => 204;
-
-    }
-
-    public class CreatedApiResponse : ApiResponse
-    {
-        public override int StatusCode => 201;
-
-        public string Location { get; init; }
-
-
-    }
-
-
 }
-
-
-
-
