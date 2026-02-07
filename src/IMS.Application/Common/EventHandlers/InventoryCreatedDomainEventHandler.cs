ed19f7b21bad0e5929b1b2edce2eebc3cf436471
Mostafa Dev <Mostafa.Medhat.0841@gmail.com>
2026-02-07 18:19:46 +0200

    feat(domain): add inventory and transaction event handlers
    implement domain events and handlers for inventory creation and transaction processing to automatically record stock history entries, improving traceability and auditability of inventory changes

---
+++ src/IMS.Application/Common/EventHandlers/InventoryCreatedDomainEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using IMS.Application.Common.Interfaces;
+using IMS.Domain.Abstractions;
+using IMS.Domain.Inventories.Domain_Events;
+using IMS.Domain.StockHistories;
+using MediatR;
+
+namespace IMS.Application.Common.EventHandlers
+{
+    internal class InventoryCreatedDomainEventHandler : INotificationHandler<InventoryCreatedDomainEvent>
+    {
+
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IDateTime dateTime;
+
+        public InventoryCreatedDomainEventHandler(IUnitOfWork unitOfWork, IDateTime dateTime)
+        {
+            this.unitOfWork = unitOfWork;
+            this.dateTime = dateTime;
+        }
+
+        public Task Handle(InventoryCreatedDomainEvent notification, CancellationToken cancellationToken)
+        {
+            var history = StockHistory.Create(notification.InventoryId, null, dateTime.UTCNow, notification.InitialStock);
+
+            unitOfWork.StockHistories.Add(history);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/IMS.Domain/Inventories/Domain Events/InventoryCreatedDomainEvent.cs
@@ -0,0 +1,21 @@
+﻿using IMS.Domain.Abstractions;
+
+namespace IMS.Domain.Inventories.Domain_Events
+{
+    public class InventoryCreatedDomainEvent : IDomainEvent
+    {
+        public InventoryCreatedDomainEvent(Guid inventoryId, int initialStock)
+        {
+            InventoryId = inventoryId;
+            InitialStock = initialStock;
+        }
+
+        public Guid InventoryId { get; init; }
+        public int InitialStock { get; init; }
+
+
+
+
+
+    }
+}
